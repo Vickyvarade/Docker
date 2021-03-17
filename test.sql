ecb19987ac6623891aeea26f90d5eee60ba103fd
Vickyvarade19 <57739743+Vickyvarade@users.noreply.github.com>
2021-03-17 10:33:25 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,18 @@
+
+
+CREATE TABLE studentData(Roll_no int PRIMARY KEY,Name varchar(30),Address varchar(40),Mobile_Number
+text(12),PAN_Number text(12));
+			
+insert into studentData(Roll_no,Name,Address,Mobile_Number,PAN_Number) values(101,'RAJ','PUNE',		  8956231458,'BOBPG7856G');
+
+insert into studentData(Roll_no,Name,Address,Mobile_Number,PAN_Number) values(102,'OM','NASHIK',7856531458,'BOBKD4856K');
+
+insert into studentData(Roll_no,Name,Address,Mobile_Number,PAN_Number) values(102,'OMKAR','NASHIK',6845531458,'JGKOB8556K');
+
+insert into studentData(Roll_no,Name,Address,Mobile_Number,PAN_Number) values(104,'RAMESH','MUMBAI',9658624558,'KFIOB6556H);
+
+insert into studentData(Roll_no,Name,Address,Mobile_Number,PAN_Number) values(105,'KUSH','MUMBAI',9658245258,'HJKDJ5264G');
+
+insert into studentData(Roll_no,Name,Address,Mobile_Number,PAN_Number) values(106,'MOHAN','PUNE',
+9658452154,'HJKGG5566H);
+
